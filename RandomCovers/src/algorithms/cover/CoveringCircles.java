efe59313e2518d9c77276255b2caf32b664cf291
ahmed <ahmed@ahmed-laptop>
2013-04-13 00:44:05 -0400

    First commit

---
+++ RandomCovers/src/algorithms/cover/CoveringCircles.java
@@ -0,0 +1,132 @@
+package algorithms.cover;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+import algorithms.voronoi.*;
+
+public class CoveringCircles {
+
+	public boolean D;
+	public double maxX, maxY; 
+	public ArrayList<Point2D> points; 
+	public ArrayList< ArrayList<Point2D> > vorSites; 
+	public double maxCoverDistance = 0.0;
+	public ArrayList<Point2D> tags; 
+	
+	public CoveringCircles() { 
+		vorSites = new ArrayList< ArrayList<Point2D> >(); 
+		points = new ArrayList<Point2D>();
+		tags = new ArrayList<Point2D>();
+
+	}
+	
+	
+	
+	
+	public void generateRandomPoints(int numberNodes, int maxX, int maxY, char r) { 
+
+		
+		this.maxX = maxX; 
+		this.maxY = maxY; 
+		
+		ArrayList<Point2D> tempPoints = new ArrayList<Point2D>();
+		
+		// generate random points, 
+		for (int i = 0; i < numberNodes; i++) { 
+			double x =  new Random().nextDouble() * maxX;
+			double y = new Random().nextDouble() * maxY; 
+			tempPoints.add(new Point2D.Double(x,y));
+			
+			if (true) {
+				System.out.printf("new point (%f, %f) \n", x, y);
+			}
+		}
+		
+		if (r == 'r') { 
+			points.clear(); 
+			points.addAll(tempPoints);
+		} else if (r == 't') { 
+			tags.clear();
+			tags.addAll(tempPoints);
+		} else {
+			System.out.println("Error: it is not specified which arrayList should be modified. options: r or t");
+		}
+
+	}
+
+	public boolean generateVoronoiSites() {
+		if (points == null) { return false; }
+		if (points.size() == 0) { return false; }
+		
+		System.out.printf("Generating Voronoi Sites for %d points \n", points.size());
+		
+		double minDistance = 1; 
+		Voronoi v = new Voronoi(minDistance);
+		
+		double[] xS = new double[points.size()];
+		double[] yS = new double[points.size()];
+		for (int i = 0; i < points.size(); i++) { 
+			xS[i] = points.get(i).getX(); 
+			yS[i] = points.get(i).getY();
+			System.out.printf("(%f,%f) ", xS[i], yS[i]);
+		}
+		
+		
+		vorSites.clear();
+		for (int i = 0; i < points.size() ; i++) { 
+			vorSites.add(new ArrayList<Point2D>());
+		}
+		
+		
+		
+		List<GraphEdge> result = v.generateVoronoi(xS, yS, 0, maxX, 0, maxY);
+		System.out.printf("voronois has %d edges \n", result.size());
+		
+		for (int i = 0; i < result.size(); i++) { 
+			GraphEdge e = result.get(i);
+			Point2D p1 = new Point2D.Double(e.x1, e.y1); 
+			Point2D p2 = new Point2D.Double(e.x2, e.y2);
+			
+			vorSites.get(e.site1).add(p1);
+			vorSites.get(e.site1).add(p2);
+			vorSites.get(e.site2).add(p1); 
+			vorSites.get(e.site2).add(p2);
+		}
+		return true; 
+	}
+
+	public double getMaxDistance() { 
+		return maxCoverDistance; 
+	}
+	
+	public void setMaxDistance() { 
+		
+		maxCoverDistance = 0.0; 
+		
+		System.out.println("Pringint table "); 
+		for (int i = 0; i < vorSites.size(); i++) { 
+			// System.out.printf("site(%d): ", i);
+			// System.out.println("pos: " + points[i] );
+			
+			for (int j = 0; j < vorSites.get(i).size(); j++) {
+				// System.out.print("\tp(" + j + "): " + vorSites.get(i).get(j) + " ");
+				double tempDistance = points.get(i).distance(vorSites.get(i).get(j)); 
+				
+				if (tempDistance > maxCoverDistance) { maxCoverDistance = tempDistance; }
+				
+				// System.out.print("\t -- dist(): " + tempDistance );
+				// System.out.println();
+			}
+			// System.out.println();
+		}
+		
+		// System.out.printf("Max Cover distance : %f ", maxCoverDistance);
+	
+	}
+	
+	
+}
---
+++ RandomCovers/src/com/test/Tester.java
@@ -0,0 +1,241 @@
+package com.test;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+import algorithms.cover.*;
+import algorithms.graph.*;
+import algorithms.voronoi.*;
+
+public class Tester {
+
+	private static int numberNodes = 70; 
+	private static int minDistance = 1; 
+	private static int xMax = 30, yMax = 30; 
+	
+	
+	// use a hash set -- from stackoverflow
+	// note that Point2D has a overridden method equals. 
+	public static void removeRedundancies(List<Point2D> l) {
+		HashSet<Point2D> hs = new HashSet<Point2D>();
+		hs.addAll(l);
+		l.clear(); 
+		l.addAll(hs);
+	}
+	
+	
+	// change precision of a double 
+	public static double changePrecision(double input, int p) { 
+		input = input * Math.pow(10,p); 
+		input = Math.ceil(input); 
+		input = input / Math.pow(10, p);
+		
+		return input; 
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CoveringCircles c = new CoveringCircles();
+		c.generateRandomPoints(10, 30,30, 'r'); 
+		
+		if (c.points.size() == 0) { System.out.println("No points "); }
+		if (! c.generateVoronoiSites() ) {System.out.println("Could not generate V sites"); } 
+		c.setMaxDistance(); 
+		
+		
+		System.out.printf("Max distance : %f ", c.maxCoverDistance );
+		// System.exit(0);
+		
+		// creating graph. 
+		UDG udg = new UDG(); 
+		udg.createGraph(c.points, c.maxCoverDistance);
+		udg.print();
+		
+		
+		
+		// is graph connected ? 
+		if (udg.isConnected()) System.out.println("Graph is connected"); 
+		else { System.out.println("Graph is not connected"); }
+	
+		
+		// Now, generate a set of points.
+		c.generateRandomPoints(20, 30, 30, 't'); 
+		udg.createBipartiteGraph(c.points, c.tags, c.maxCoverDistance);
+		udg.printBipartiteGraph();
+		if (udg.isBipCovered()) System.out.println("Bibpartite graph is covering "); 
+		else System.out.println("Bibpartite graph is not covering");
+		
+		// converting bipartite into lists; -- to make it ready for set-cover
+		udg.convertBipartiteToList();
+		udg.printBipartiteList();  
+		
+		ArrayList<Integer> tags = new ArrayList<Integer>(); 
+		for (int i = 0; i < c.tags.size(); i++) tags.add(i); 
+		
+		ArrayList<Integer> sets = setCover(udg.listBipGraph, tags);
+		for (int i = 0; i < sets.size(); i++) { 
+			System.out.printf("%d ", sets.get(i));
+		}
+		
+	}
+	
+	public static ArrayList<Integer> setCover(ArrayList<ArrayList<Integer>> listBipGraph, ArrayList<Integer> tags) {
+		
+		
+		System.out.println("Set cover ");
+		
+		ArrayList<Integer> sets = new ArrayList<Integer>();
+		ArrayList<Boolean> markedElements = new ArrayList<Boolean>();
+		for (int i = 0; i < tags.size(); i++) markedElements.add(false); 
+		
+		
+		int iteration = 1; 
+		while (! allMarked(markedElements)) {
+			
+			System.out.printf("Starting iteration %d \n" , iteration);
+			
+			int maxIndex = findMaxSizeList(listBipGraph); 
+			
+			
+			
+			
+			System.out.printf("Adding %d \n", maxIndex);
+			sets.add(maxIndex);
+			
+	
+			for (int i = 0; i < listBipGraph.get(maxIndex).size(); i++) { 
+				markedElements.set(listBipGraph.get(maxIndex).get(i), true);
+				System.out.printf("Marking element %d \n", listBipGraph.get(maxIndex).get(i));
+			}
+			
+			// do the intersection.
+			for (int i = 0; i< listBipGraph.size(); i++) {
+				// removeAll(listBipGraph.get(maxIndex), listBipGraph.get(i));
+				ArrayList<Integer> re = removeElements(listBipGraph.get(maxIndex), listBipGraph.get(i)); 
+				listBipGraph.set(i, re);
+			}
+			
+			// After deletion: 
+			System.out.println("After deletion");
+			for (int i = 0; i < listBipGraph.size(); i++) { 
+				for (int j = 0; j < listBipGraph.get(i).size(); j++) {
+					System.out.printf("%d ", listBipGraph.get(i).get(j));
+				}
+				System.out.println();
+			}
+
+			iteration ++;
+		}
+		
+		System.out.println("going out");
+		
+		return sets;
+	}
+	
+	// TODO: (Fix) This is a buggy implementation 
+	private static void removeAll(ArrayList<Integer> smallList,
+			ArrayList<Integer> bigList) {
+
+			System.out.println("removing "); 
+			for (int i = 0; i < smallList.size(); i++) System.out.printf("%d ", smallList.get(i));
+			System.out.println();
+			
+			System.out.println("from : "); 
+			for (int i = 0; i < bigList.size(); i++) System.out.printf("%d ", bigList.get(i));
+			System.out.println();
+			
+			// TODO: the error is here: This is not efficient 
+			for (int i = 0; i < smallList.size(); i++ ) {
+				bigList.remove(smallList.get(i));
+			}
+		
+			
+			System.out.println("result is");
+			for (int i = 0; i < bigList.size(); i++) System.out.printf("%d ", bigList.get(i));
+			System.out.println();
+	}
+
+	
+	private static ArrayList<Integer> removeElements(ArrayList<Integer> smallList, ArrayList<Integer> bigList) { 
+		Collections.sort(bigList);
+		Collections.sort(smallList);
+		
+		System.out.println("removing ");
+		for (int i = 0; i < smallList.size(); i++) System.out.printf("%d ", smallList.get(i));
+		System.out.println();
+
+		
+		System.out.println("from : "); 
+		for (int i = 0; i < bigList.size(); i++) System.out.printf("%d ", bigList.get(i));
+		System.out.println();
+		
+		if (smallList.size() == 0) { return bigList; }
+		
+		ArrayList<Integer> result = new ArrayList<Integer>(); 
+		int i = 0, j = 0;
+		while ( i < bigList.size() && j < smallList.size())  { 
+			
+			System.out.printf("Comparing (s(%d)): %d to l(%d): %d \n", j, smallList.get(j), i, bigList.get(i));
+			
+			if (smallList.get(j) > bigList.get(i)) { result.add(bigList.get(i)); System.out.printf("Add %d \n", bigList.get(i));  i ++; }
+			else if (smallList.get(j) == bigList.get(i)) { i++ ; j++; } 
+			else { j++; }
+			
+			 
+		}
+		
+		for (;i < bigList.size(); i++)
+			result.add(bigList.get(i)); 
+		
+		// System.exit(0);
+		return result; 
+	}
+	
+
+	private static boolean allMarked(ArrayList<Boolean> markedElements) {
+
+		for (int i = 0; i < markedElements.size(); i++ ) { 
+			if (markedElements.get(i) == false) { System.out.printf("element %d is not marked yet \n", i); return false; }
+		}
+		
+		return true;
+	}
+
+
+	private static int findMaxSizeList(
+			ArrayList<ArrayList<Integer>> listBipGraph) {
+
+		int max = 0, maxIndex = -1;
+		for (int i = 0; i < listBipGraph.size(); i++) {
+			if (listBipGraph.get(i).size() > max) { 
+				max = listBipGraph.get(i).size();
+				maxIndex = i; 
+			}
+		}
+		
+		if (maxIndex == -1) { 
+			System.out.println("There must be an error in findMaxSizeList");
+			System.exit(0);
+		}
+		
+		
+		return maxIndex;
+	}
+
+
+	public static Point2D.Double shrinkPoint(Point2D.Double c, Point2D.Double po,  double scale) { 
+		Point2D.Double pn = new Point2D.Double(); 
+		pn.x = c.x + (c.x - po.x) * scale; 
+		pn.y = c.y + (c.y - po.y) * scale; 
+		
+		return pn; 
+	}
+
+
+}
